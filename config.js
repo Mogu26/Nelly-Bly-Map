cd0a8c0d75afa33f8b93eb426908d5d7fc8dced4
Mogu26 <mogui2601@gmail.com>
2026-03-02 22:23:54 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Jersey City, Nueva Jersey, EE.UU.',
-            image: './assets/nelly.bly.webp',
+            image: 'https://media.revistavanityfair.es/photos/60e84e97bf8d45dd8c6f7815/master/w_1600,c_limit/19788.jpg',
             description: '<b><u>14 DE NOVIEMBRE, 1889</u></b><br> ' +
                         'Nellie Bly fue una audaz reportera del <b>New York World</b> y pionera del periodismo de investigación, que desafió ' +
                         'las normas de género de su época gracias a reportajes encubiertos y más tarde mediante una audaz hazaña.' +
@@ -44,7 +44,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Londres, Reino Unido',
-            image: './assets/victoria.webp',
+            image: 'https://www.heinzhistorycenter.org/wp-content/uploads/2023/01/4_4a15881u-768x615.webp',
             description: '<b><u>22 DE NOVIEMBRE, 1889</u></b><br> '+
             'Tras siete días cruzando el Atlántico en el Augusta Victoria, desembarcó en Southampton y viajó a Londres; '+
             ' allí solo tuvo tiempo para enviar telegramas a su editor y apenas descansar antes de cruzar el Canal de la Mancha esa misma noche.', 
@@ -65,7 +65,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Amiens, Francia',
-            image: './assets/verne (1).jpeg',
+            image: 'https://publicacionesarenas.com/wp-content/uploads/2022/02/01-retrato-julio-verne_be9f1997_1325x2000-1018x1536.jpeg',
             description: '<b><u>23 DE NOVIEMBRE, 1889</u></b><br> '+
             'Se desvió de su ruta original para visitar al mismísimo Julio Verne, autor de <i>La Vuelta al Mundo en 80 días</i>, obra que inspiraría esta travesía.'+
             ' El escritor quedó impresionado por su determinación y juventud, '+
@@ -108,7 +108,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Puerto Said, Suez, Egipto',
-            image: './assets/said.webp',
+            image: 'https://64.media.tumblr.com/tumblr_lxuugbtaGo1qd7ygho1_1280.jpg',
             description: '<b><u>27 DE NOVIEMBRE, 1889</u></b><br> '+
             'Entró a Oriente por el Canal de Suez, donde aprovechó una breve parada para bajar a tierra y comprar frutas. Allí estuvo observando '+
             'con curiosidad el bullicio del mercado egipcio antes de que el barco reemprendiera la marcha.', 
@@ -129,7 +129,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Adén, Yemen',
-            image: './assets/aden2.webp',
+            image: 'https://www.prints-online.com/p/164/camel-market-aden-yemen-23440712.jpg.webp',
             description: '<b><u>3 DE DICIEMBRE, 1889</u></b><br> '+
             'En la parada de la península arábiga, Bly describió el calor sofocante y la aridez del paisaje, pero se mostró fascinada por la'+
             ' habilidad de los buceadores locales que rodeaban el barco pidiendo monedas.',
@@ -150,7 +150,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Colombo, Sri Lanka',
-            image: './assets/colombo2.webp',
+            image: 'https://www.heinzhistorycenter.org/wp-content/uploads/2023/01/8_MountLaviniaHotel-768x492.webp',
             description: '<b><u>8 DE DICIEMBRE, 1889</u></b><br> '+
             'Llegó a Ceilán (Sri Lanka) y sufrió su mayor pesadilla: un retraso de cinco días por problemas logísticos del barco. Intentó '+
             'calmar sus nervios paseando en coche de caballos y conociendo los templos locales.', 
@@ -171,7 +171,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Penang, Malasia',
-            image: './assets/penang.webp',
+            image: 'https://www.heinzhistorycenter.org/wp-content/uploads/2023/01/10_ChineseCouplefromPenang-jpg.webp',
             description: '<b><u>16 DE DICIEMBRE, 1889</u></b><br> '+
             'En la costa de Malasia, Bly quedó impactada por la mezcla de culturas y el aroma a especias; fue una de las paradas donde más'+
             ' consciente fue de lo lejos que estaba de casa, pero el tiempo seguía siendo su única prioridad.', 
@@ -192,7 +192,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Singapur',
-            image: './assets/bisland.webp',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Elizabeth_Bisland_circa1891.jpg',
             description: '<b><u>18 DE DICIEMBRE, 1889</u></b><br> '+
             'En esta escala compró a su famoso compañero de viaje, un mono hambriento que la acompañaría el resto del trayecto. Fue aquí donde descubrió con '+
             'sorpresa de que otra periodista, <b>Elizabeth Bisland</b>, estaba compitiendo contra ella en sentido contrario.', 
@@ -213,7 +213,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '•  Hong Kong',
-            image: './assets/mono.png',
+            image: 'https://a.1stdibscdn.com/paul-gervais-prints-works-on-paper-the-monkey-original-lithograph-by-paul-gervais-1854-for-sale/a_6503/a_92810621638619763817/T_128099_paul_gervais_the_monkey_1_master.jpeg?disable=upscale&auto=webp&quality=60&width=1400',
             description: '<b><u>23 DE DICIEMBRE, 1889</u></b><br> '+
             'Llegó en plena temporada de monzones y se enfrentó a un clima terrible. Pese a las tormentas, mantuvo el ánimo alto tras descubrir que, '+
             'pese a los retrasos anteriores, todavía iba por delante del récord de Phileas Fogg.', 
@@ -234,7 +234,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Cantón, China',
-            image: './assets/canton.jpg',
+            image: 'https://images.imagerenderer.com/images/artworkimages/mediumlarge/1/canton-street-scene-19th-century-british-library.jpg',
             description:  '<b><u>25 DE DICIEMBRE, 1889</u></b><br> '+
             'Pasó el día de Navidad explorando la ciudad china, donde visitó un templo y un mercado local. Aunque le impresionó la densidad de población'+
             ' y la arquitectura, se sintió aliviada al regresar a la seguridad del barco para seguir su ruta.', 
@@ -255,7 +255,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Yokohama, Japón',
-            image: './assets/yokohama.jpg',
+            image: 'https://images.metmuseum.org/CRDImages/as/original/DP148133.jpg',
             description:  '<b><u>2 DE ENERO, 1890</u></b><br> '+
             'Comenzó el año nuevo en Japón, un país que adoró por su orden y belleza. Fue aquí donde empezó a sentir la nostalgia del hogar,'+
             ' sabiendo que solo le quedaba cruzar el inmenso Pacífico para ganar la apuesta.', 
@@ -276,7 +276,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  San Francisco, Estados Unidos',
-            image: './assets/train.webp',
+            image: 'https://i.ebayimg.com/images/g/CqgAAOSwOBhiqTyi/s-l1600.webp',
             description: '<b><u>21 DE ENERO, 1890</u></b><br> '+
             'Tras una travesía oceánica marcada por terribles tormentas que casi detienen el barco, atracó en EE. UU. Fue recibida como una heroína nacional'+
             ' y subió a un tren especial fletado exclusivamente para que cruzara el país sin paradas.', 
@@ -297,7 +297,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '•  Nueva Jersey, Estados Unidos',
-            image: './assets/reception.jpg',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Nellie_Bly_Reception_1890.jpg/1280px-Nellie_Bly_Reception_1890.jpg',
             description: '<b><u>25 DE ENERO, 1890</u></b><br> '+
             'Cruzó la meta en Nueva Jersey a las <b>3:51 p.m.</b>, completando la vuelta al mundo en <b>72 días, 6 horas y 11 minutos </b>. Miles de personas la vitorearon'+
             ' al bajar del tren, celebrando que una mujer en solitario había derrotado a la ficción.', 
@@ -362,8 +362,6 @@ var config = {
                         '<br>' +
                         '<li><b>British Library.</b> <i>Canton street scene, 19th century</i>. <a href="https://images.imagerenderer.com/images/artworkimages/mediumlarge/1/canton-street-scene-19th-century-british-library.jpg" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Fotografía]</a>.</li>' +
                         '<br>' +
-                        '<li><b>El Español (2016).</b> <i>Retrato histórico de Nellie Bly</i>. <a href="https://s2.elespanol.com/2016/05/20/cultura/historia-mujeres-cultura_126247917_5164887_1706x1280.jpg" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Fotografía]</a>.</li>' +
-                        '<br>' +
                         '<li><b>Heinz History Center.</b> <i>Chinese couple from Penang</i>. <a href="https://www.heinzhistorycenter.org/wp-content/uploads/2023/01/10_ChineseCouplefromPenang-jpg.webp" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Fotografía]</a>.</li>' +
                         '<br>' +
                         '<li><b>Heinz History Center.</b> <i>Mount Lavinia Hotel, Ceylon</i>. <a href="https://www.heinzhistorycenter.org/wp-content/uploads/2023/01/8_MountLaviniaHotel-768x492.webp" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Fotografía]</a>.</li>' +
@@ -382,11 +380,13 @@ var config = {
                         '<br>' +
                         '<li><b>Vanity Fair (2018).</b> <i>Retrato de Nellie Bly</i>. <a href="https://www.revistavanityfair.es/poder/articulos/nellie-bly-periodismo-manicomio/29879" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Fotografía]</a>.</li>' +
                         '<br>' +
+                        '<li><b>Wikipedia (2024).</b> <i>Elizabeth Bisland</i>. La periodista que compitió contra Bly en la carrera alrededor del mundo. <a href="https://es.wikipedia.org/wiki/Elizabeth_Bisland" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Biografía]</a>.</li>' +
+                        '<br>' +
                         '<li><b>Wikipedia.</b> <i>Tablero del juego "Around the World in 72 Days"</i>. <a href="https://en.wikipedia.org/wiki/Around_the_World_in_Seventy-Two_Days" target="_blank" style="color: #5a1a1a; font-weight: bold;">[Imagen]</a>.</li>' +
                     '</ul>'+
                 '</details> ',
                 
-                image: './assets/game.jpg',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Around_the_World_in_Seventy-Two_Days_board_game.jpg',
                 location: {
                 center: [-74.40101, 40.14991],
                 zoom: 8.23,
